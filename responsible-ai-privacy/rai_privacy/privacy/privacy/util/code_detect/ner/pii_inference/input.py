c1e5c08e425e0ef13f0a58c9dd4e16242bdd1f81
Thenmozhi Krishnan <thenmozhi.krishnan@ad.infosys.com>
2024-09-19 14:43:08 +0530

    commit

--- responsible-ai-privacy/rai_privacy/privacy/privacy/util/code_detect/ner/pii_inference/input.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoModelForTokenClassification, AutoTokenizer
-import torch
-secretkey= "cmVnrGtuOjAxOjE3MjEyODUwMjg6M0RrNjVMVGZEaGd6T0RiZ09FR3M5MEV5Tk0z"
-ipadress= "10.224.233.18"
-email= "abhi@gmail.com"
-useraccountname="Abhishek"
